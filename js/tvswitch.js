777eee4aba350875f02cb508b55cae481f02df11
Wartburg101 <wartburgerik@outlook.com>
2023-11-12 01:03:31 +0100

    working on scroll animations + changed about

--- js/tvswitch.js
+++ js/tvswitch.js
@@ -6,18 +6,20 @@ const switchtv = document.getElementById("tvstatic");
 
 let toggle = true;
 
+//Listen for click
 switchtv.addEventListener("click", function () {
+  //Make toggle false if true, and true if false
   toggle = !toggle;
   if (!toggle) {
     document.getElementById("tvstatic").style.opacity = "0%";
-
+    document.getElementById("tvshowreel").style.opacity = "100%";
     document.getElementById("tvshowreel").classList.remove('tvframeanimation');
     void document.getElementById("tvshowreel").offsetWidth;
     document.getElementById("tvshowreel").classList.add('tvframeanimation');
     
   } else {
     document.getElementById("tvstatic").style.opacity = "100%";
-    
+    document.getElementById("tvshowreel").style.opacity = "0%";
     switchtv.classList.remove('tvframeanimation');
     void switchtv.offsetWidth;
     switchtv.classList.add('tvframeanimation');
