c89fdf5f032dd7a8cbf10797a059529442cd8da9
Wartburg101 <wartburgerik@outlook.com>
2023-11-19 12:30:54 +0100

    more fix

--- js/scroll.js
+++ js/scroll.js
@@ -2,30 +2,61 @@ let abouttitle = document.getElementById("abouttitle");
 let aboutsubtext = document.getElementById("aboutsubtext");
 let aboutbody = document.getElementById("aboutbodytext");
 
-/*Navigation*/ 
-let navbuttons = document.getElementsByClassName("navbuttons");
+/*Navindicator*/ 
+let sectiontitle = document.getElementById("navindicator")
 
 window.onscroll = function() {
     /*Aboutpage scroll */
-let scrollLimit = 400;
-if(window.scrollY >= scrollLimit){
-    abouttitle.style.opacity = "100%"
-    abouttitle.style.transform = "none"
+let scrollLimit1 = 400;
+/*Workpage scroll*/
+let scrollLimit2 = 1000;
 
-    aboutsubtext.style.opacity = "100%"
-    aboutsubtext.style.transform = "none"
+/*Find Out where the user is*/
 
-    aboutbody.style.opacity = "100%"
-
-    
+if(window.scrollY >= scrollLimit2){
+    scrollAnimations("work");
+}
+else if(window.scrollY >= scrollLimit1){
+    scrollAnimations("about");
 }
 else{
-    abouttitle.style.opacity = "0%"
-    abouttitle.style.transform = "translateX(-20%)"
-
-    aboutsubtext.style.opacity = "0%"
-    aboutsubtext.style.transform = "translateX(-20%)"
-
-    aboutbody.style.opacity = "0%"
+    scrollAnimations("home");
 }
+}
+
+function scrollAnimations(position){
+
+    if(position == "home"){
+        
+        abouttitle.style.opacity = "0%"
+        abouttitle.style.transform = "translateX(-20%)"
+        aboutsubtext.style.opacity = "0%"
+        aboutsubtext.style.transform = "translateX(-20%)"
+        aboutbody.style.opacity = "0%"
+
+        sectiontitle.style.transform = "translateY(0%)"
+    }
+    if(position == "about"){
+        
+        abouttitle.style.opacity = "100%"
+        abouttitle.style.transform = "none"
+        aboutsubtext.style.opacity = "100%"
+        aboutsubtext.style.transform = "none"
+        aboutbody.style.opacity = "100%"
+
+        sectiontitle.style.transform = "translateY(-25%)"
+    }
+    if(position == "work"){
+        "translateY(-50%)"
+        abouttitle.style.opacity = "0%"
+        abouttitle.style.transform = "translateX(-20%)"
+        aboutsubtext.style.opacity = "0%"
+        aboutsubtext.style.transform = "translateX(-20%)"
+        aboutbody.style.opacity = "0%"
+
+        sectiontitle.style.transform = "translateY(-50%)"
+    }
+    else{
+
+    }
 }
